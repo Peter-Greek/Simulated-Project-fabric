8598b0d3fb0f1f84cbea11a1074ea35c0858e766
Peter Greek <xerxes468893@gmail.com>
2026-09-04 20:46:38 -0400

    Drive helm with normal vehicle controls

--- simulated/fabric/src/main/java/dev/simulated_team/simulated/fabric/SimulatedFabricClient.java
+++ simulated/fabric/src/main/java/dev/simulated_team/simulated/fabric/SimulatedFabricClient.java
@@ -1,33 +1,21 @@
 package dev.simulated_team.simulated.fabric;
 
-import com.mojang.blaze3d.platform.InputConstants;
 import com.simibubi.create.content.contraptions.ControlledContraptionEntity;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.minecraft.client.KeyMapping;
+import net.minecraft.core.Direction;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
-import org.lwjgl.glfw.GLFW;
 
 /** Client-only hooks for the Homestead Fabric port. */
 public final class SimulatedFabricClient implements ClientModInitializer {
-    private static KeyMapping flightToggle;
-    private static KeyMapping flightForward;
-    private static KeyMapping flightBack;
-    private static KeyMapping flightLeft;
-    private static KeyMapping flightRight;
-    private static KeyMapping flightUp;
-    private static KeyMapping flightDown;
     private static boolean flightControlActive;
 
     @Override
     public void onInitializeClient() {
-        registerFlightKeys();
-
         ClientPlayNetworking.registerGlobalReceiver(
                 SimulatedFabricNetworking.CONTRAPTION_POSITION,
                 (client, handler, buffer, responseSender) -> {
@@ -35,14 +23,15 @@ public final class SimulatedFabricClient implements ClientModInitializer {
                     final double x = buffer.readDouble();
                     final double y = buffer.readDouble();
                     final double z = buffer.readDouble();
+                    final float angle = buffer.readFloat();
 
                     client.execute(() -> {
                         if (client.level == null) {
                             return;
                         }
                         if (client.level.getEntity(entityId) instanceof final ControlledContraptionEntity entity) {
-                            // ControlledContraptionEntity deliberately ignores vanilla lerp/teleport
-                            // updates. Apply the authoritative server position directly on the client.
+                            entity.setRotationAxis(Direction.Axis.Y);
+                            entity.setAngle(angle);
                             entity.setPos(x, y, z);
                         }
                     });
@@ -66,60 +55,37 @@ public final class SimulatedFabricClient implements ClientModInitializer {
                 flightControlActive = false;
                 return;
             }
-
-            while (flightToggle.consumeClick()) {
-                final FriendlyByteBuf toggle = PacketByteBufs.create();
-                ClientPlayNetworking.send(SimulatedFabricNetworking.FLIGHT_CONTROL_TOGGLE, toggle);
-            }
-
             if (!flightControlActive) {
                 return;
             }
 
             int inputMask = 0;
-            if (flightForward.isDown()) {
-                inputMask |= SimulatedFabricNetworking.FLIGHT_FORWARD;
-            }
-            if (flightBack.isDown()) {
-                inputMask |= SimulatedFabricNetworking.FLIGHT_BACK;
-            }
-            if (flightLeft.isDown()) {
-                inputMask |= SimulatedFabricNetworking.FLIGHT_LEFT;
-            }
-            if (flightRight.isDown()) {
-                inputMask |= SimulatedFabricNetworking.FLIGHT_RIGHT;
-            }
-            if (flightUp.isDown()) {
-                inputMask |= SimulatedFabricNetworking.FLIGHT_UP;
-            }
-            if (flightDown.isDown()) {
-                inputMask |= SimulatedFabricNetworking.FLIGHT_DOWN;
+            if (client.screen == null) {
+                if (client.options.keyUp.isDown()) {
+                    inputMask |= SimulatedFabricNetworking.FLIGHT_FORWARD;
+                }
+                if (client.options.keyDown.isDown()) {
+                    inputMask |= SimulatedFabricNetworking.FLIGHT_BACK;
+                }
+                if (client.options.keyLeft.isDown()) {
+                    inputMask |= SimulatedFabricNetworking.FLIGHT_LEFT;
+                }
+                if (client.options.keyRight.isDown()) {
+                    inputMask |= SimulatedFabricNetworking.FLIGHT_RIGHT;
+                }
+                if (client.options.keyJump.isDown()) {
+                    inputMask |= SimulatedFabricNetworking.FLIGHT_UP;
+                }
+                if (client.options.keySprint.isDown()) {
+                    inputMask |= SimulatedFabricNetworking.FLIGHT_DOWN;
+                }
             }
 
             final FriendlyByteBuf input = PacketByteBufs.create();
             input.writeByte(inputMask);
-            input.writeFloat(client.player.getYRot());
             ClientPlayNetworking.send(SimulatedFabricNetworking.FLIGHT_INPUT, input);
         });
 
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> flightControlActive = false);
-    }
-
-    private static void registerFlightKeys() {
-        flightToggle = registerKey("key.simulated.flight_toggle", GLFW.GLFW_KEY_G);
-        flightForward = registerKey("key.simulated.flight_forward", GLFW.GLFW_KEY_UP);
-        flightBack = registerKey("key.simulated.flight_back", GLFW.GLFW_KEY_DOWN);
-        flightLeft = registerKey("key.simulated.flight_left", GLFW.GLFW_KEY_LEFT);
-        flightRight = registerKey("key.simulated.flight_right", GLFW.GLFW_KEY_RIGHT);
-        flightUp = registerKey("key.simulated.flight_up", GLFW.GLFW_KEY_PAGE_UP);
-        flightDown = registerKey("key.simulated.flight_down", GLFW.GLFW_KEY_PAGE_DOWN);
-    }
-
-    private static KeyMapping registerKey(final String translationKey, final int defaultKey) {
-        return KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                translationKey,
-                InputConstants.Type.KEYSYM,
-                defaultKey,
-                "key.categories.simulated"));
     }
 }
